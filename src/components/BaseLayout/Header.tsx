ada6517e24dd905d834467a2e1b0eb9c7a428d6c
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-05 16:17:25 +0900

    ヘッダーにデフォルトアイコンを表示

--- src/components/BaseLayout/Header.tsx
+++ src/components/BaseLayout/Header.tsx
@@ -1,7 +1,8 @@
 "use client"
 import { loginGoogle } from "@/auth/client/login"
 import { useUser } from "@/auth/client/useUser"
-import { Button, Flex, Space } from "@mantine/core"
+import { Box, Button, Container, Flex, Loader, Space } from "@mantine/core"
+import Image from "next/image"
 import { FC } from "react"
 import Logo from "../Logo"
 import styles from "./Header.module.css"
@@ -13,21 +14,32 @@ const Header: FC<HeaderProps> = () => {
     console.log("header", status, user)
 
     return (
-        <header className={styles.header}>
-            <Flex align="center" h="100%">
-                <Logo />
-                <Space style={{ flex: "1 1" }} />
-                {status}
-                {status === "logined"
-                    ? <>{user.name}</>
-                    : <>
-                        <Button mr="sm" size="xs" onClick={() => loginGoogle()}>
-                            ログイン
-                        </Button>
-                    </>
-                }
-            </Flex>
-        </header>
+        <Box component="header" className={styles.header}>
+            <Container h="100%">
+                <Flex align="center" h="100%">
+                    <Logo />
+                    <Space style={{ flex: "1 1" }} />
+                    {
+                        status === "loading"
+                            ? <Loader size="xs" />
+                            : status === "logined"
+                                ? <Image
+                                    className={styles.icon}
+                                    src={user.image ?? "/default-user-icon.png"}
+                                    alt={user.name ?? "不明なユーザ"}
+                                    width={30}
+                                    height={30}
+                                    unoptimized
+                                />
+                                : <>
+                                    <Button mr="sm" size="xs" onClick={() => loginGoogle()}>
+                                        ログイン
+                                    </Button>
+                                </>
+                    }
+                </Flex>
+            </Container>
+        </Box>
     )
 }
 

b31a730fc101b4e380038a0c089ee563bd63c230
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-10 06:34:45 +0900

    メニューを修正

--- src/components/BaseLayout/Header.tsx
+++ src/components/BaseLayout/Header.tsx
@@ -1,17 +1,20 @@
 "use client"
-import { loginGoogle } from "@/auth/client/login"
+import { loginGoogle, logout } from "@/auth/client/login"
 import { useUser } from "@/auth/client/useUser"
-import { Box, Button, Container, Flex, Loader, Space } from "@mantine/core"
+import { Box, Button, Container, Flex, Loader, Menu, Space } from "@mantine/core"
+import { User } from "next-auth"
 import Image from "next/image"
+import Link from "next/link"
 import { FC } from "react"
+import { useQuery } from "react-query"
 import Logo from "../Logo"
 import styles from "./Header.module.css"
+import { getProfileByUserId } from "./actions"
 
 interface HeaderProps {
 }
 const Header: FC<HeaderProps> = () => {
     const { status, user } = useUser()
-
     return (
         <Box component="header" className={styles.header}>
             <Container h="100%">
@@ -22,14 +25,7 @@ const Header: FC<HeaderProps> = () => {
                         status === "loading"
                             ? <Loader size="xs" />
                             : status === "logined"
-                                ? <Image
-                                    className={styles.icon}
-                                    src={user.image ?? "/default-user-icon.png"}
-                                    alt={user.name ?? "不明なユーザ"}
-                                    width={30}
-                                    height={30}
-                                    unoptimized
-                                />
+                                ? <Avatar user={user} />
                                 : <>
                                     <Button mr="sm" size="xs" onClick={() => loginGoogle()}>
                                         ログイン
@@ -41,5 +37,42 @@ const Header: FC<HeaderProps> = () => {
         </Box>
     )
 }
-
 export default Header
+
+interface AvatarProps {
+    user: User
+}
+const Avatar: FC<AvatarProps> = ({ user }) => {
+    const { data: profile, isLoading } = useQuery({
+        queryKey: ["profile", user?.id],
+        queryFn: async () => {
+            const userId = user?.id
+            if (!userId) return
+            return await getProfileByUserId(userId)
+        },
+    })
+    return (
+        <Menu>
+            <Menu.Target>
+                <Image
+                    className={styles.icon}
+                    src={user.image ?? "/default-user-icon.png"}
+                    alt={user.name ?? "不明なユーザ"}
+                    width={30}
+                    height={30}
+                    unoptimized
+                />
+            </Menu.Target>
+            <Menu.Dropdown>
+                <Menu.Label>{user.name}</Menu.Label>
+                <Menu.Item component={Link} href="/profile/edit">
+                    {(!isLoading && !profile) ? "プロフィールを登録" : "プロフィールを編集"}
+                </Menu.Item>
+                <Menu.Divider />
+                <Menu.Item onClick={() => logout()}>
+                    ログアウト
+                </Menu.Item>
+            </Menu.Dropdown>
+        </Menu>
+    )
+}
---
+++ src/components/BaseLayout/actions.ts
@@ -0,0 +1,4 @@
+"use server"
+import { getProfile } from "@/auth/server/getProfile"
+
+export const getProfileByUserId = getProfile

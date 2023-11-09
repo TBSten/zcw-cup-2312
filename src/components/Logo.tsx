2cc1d10906f96b86da34a5e3a8470d91739b4145
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-10 07:34:42 +0900

    ロゴの修正

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,14 @@
+import { Text } from "@mantine/core"
+import Link from "next/link"
 import { FC } from "react"
 
 interface LogoProps {
 }
 const Logo: FC<LogoProps> = () => {
     return (
-        <div>
+        <Text component={Link} href="/">
             ZutomayoCard大会
-        </div>
+        </Text>
     )
 }
 
